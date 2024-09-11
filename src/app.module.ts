4b9f12b2e62ac7e73fbfd0efcc8c242c25812b2a
Nguyễn Kim Quốc Nam <namnkq2603@gmail.com>
2024-09-11 17:09:13 +0700

    #2: setup mongodb & using .env file with configService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './modules/products/product.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
